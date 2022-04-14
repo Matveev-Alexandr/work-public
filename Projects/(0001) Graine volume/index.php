cfab3a8bf7102f5e85fcac8c720ca78101a5c839
Matveev-Alexandr <matveev1au@gmail.com>
2022-04-14 09:57:39 +0300

    (0001) Graine volume (v.01.001)

--- Projects/(0001) Graine volume/index.php
+++ Projects/(0001) Graine volume/index.php
@@ -2,8 +2,8 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <link href="normalize.css" rel="stylesheet">
     <title>Расчёт геометрии ямы</title>
+    <link href="normalize.css" rel="stylesheet">
     <link href="styles.css" rel="stylesheet">
     <?php
 
@@ -77,15 +77,17 @@
             </div>
         <?php endforeach; ?>
         <?php foreach ($arrayAngle as $item): ?>
-            <p class="fieldA<?= $item ?>">&#945<sub><?= $item ?></sub>=<?= $total['A' . $item] ?></p>
+        <?php if (!empty($total['A' . $item])): ?>
+            <p class="fieldA<?= $item ?>">&#945<sub><?= $item ?></sub>=<?= $total['A' . $item] ?>&#176</p>
+            <?php endif; ?>
         <?php endforeach; ?>
-        <p class="text-description"><u>Параметры объёма будут:</u><br>V=<?= str_replace('.', ',', $total['V']) ?> м<sup>3</sup>
-            или Vт=<?= $total['Vt'] ?> т при плотности <?= str_replace('.', ',', $_GET['sizer']) ?> т/м<sup>3</sup>
-            (каждый 0,1 м размера "h" добавляет A&#xD7;B=<?= str_replace('.', ',', $total['ABm']) ?> м<sup>3</sup>
-            объёма или A&#xD7;B=<?= $total['ABt'] ?> т)</p>
         <button class="fieldBut1">Расчитать</button>
         <?php if (empty(!$total['V'])): ?>
             <p class="fieldAComplet textGreen">РАСЧЁТ ВЫПОЛНЕН</p>
+            <p class="text-description"><u>Параметры объёма будут:</u><br>V=<?= str_replace('.', ',', $total['V']) ?> м<sup>3</sup>
+                или Vт=<?= $total['Vt'] ?> т при плотности <?= str_replace('.', ',', $_GET['sizer']) ?> т/м<sup>3</sup>
+                (каждый 0,1 м размера "h" добавляет A&#xD7;B=<?= str_replace('.', ',', $total['ABm']) ?> м<sup>3</sup>
+                объёма или A&#xD7;B=<?= $total['ABt'] ?> т)</p>
         <?php else: ?>
             <p class="fieldAComplet textRed">РАСЧЁТ НЕ ВЫПОЛНЕН ИЛИ ДАННЫЕ НЕ ВЕРНЫ</p>
         <?php endif; ?>
